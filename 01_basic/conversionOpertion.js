b2692e93029676225e033875e80c254d52f16f06
Muhammad Boota <154687034+RanaBoota@users.noreply.github.com>
2025-09-02 08:39:29 +0000

    Conversion OPeration

---
+++ 01_basic/conversionOpertion.js
@@ -0,0 +1,10 @@
+// let score="33abs";
+
+// console.log(typeof score);
+
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+
+let value=3;
+let negvalue=-3;
+console.log(negvalue);
