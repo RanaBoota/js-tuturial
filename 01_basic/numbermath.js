fb9df06e43aa4b5bfe70c6c1de81062e47b9342c
Muhammad Boota <154687034+RanaBoota@users.noreply.github.com>
2025-09-02 11:02:20 +0000

    Number and Math

---
+++ 01_basic/numbermath.js
@@ -0,0 +1,26 @@
+const score=400;
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+const anotherNumber=123.875
+console.log(anotherNumber.toPrecision(4));
+
+const hunderds=1000000
+console.log(hunderds.toLocaleString());
+
+// +++++++ Math +++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.8));
+console.log(Math.pow(4, 5));
+
+
+
+
