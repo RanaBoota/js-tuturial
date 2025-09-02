619c0dd8c5e70c286e18302c98ece7e145f3fabe
Muhammad Boota <154687034+RanaBoota@users.noreply.github.com>
2025-09-02 11:07:08 +0000

    opertions

--- 01_basic/conversionOpertion.js
+++ 01_basic/conversionOpertion.js
@@ -5,6 +5,14 @@
 // let valueInNumber=Number(score);
 // console.log(typeof valueInNumber);
 
-let value=3;
-let negvalue=-3;
-console.log(negvalue);
+// let value=3;
+// let negvalue=-3;
+// console.log(negvalue);
+
+
+// Strings
+let name="Tayyab";
+let age="23";
+let course="BSSE";
+console.log(`The name is ${name} and age of ${age} and study the course is ${course}`);
+
