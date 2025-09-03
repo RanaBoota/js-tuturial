979ff09500a45d32e10329fd65f32f03b6a0a6bb
Muhammad Boota <154687034+RanaBoota@users.noreply.github.com>
2025-09-03 10:28:12 +0000

    Scopes

---
+++ 01_basic/scope.js
@@ -0,0 +1,21 @@
+function one(params) {
+    const username="Boota"
+    function two(params) {
+        const website="youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+}
+// one()
+console.log(addone(5));
+
+function addone(num) {
+    return num+1
+}
+
+console.log(addtwo(5))
+const addtwo=function (num) {
+    return num+1
+}
