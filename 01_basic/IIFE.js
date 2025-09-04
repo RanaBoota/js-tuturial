340633fc2747f943757c025a6c46aa2b04906718
Muhammad Boota <154687034+RanaBoota@users.noreply.github.com>
2025-09-04 05:20:21 +0000

    IIFE

---
+++ 01_basic/IIFE.js
@@ -0,0 +1,11 @@
+// IFFE : Immediately Invoked Function Expression
+
+(function chai(params) {
+    console.log(`DB Connected`);
+    
+})();
+
+((name)=>{
+    console.log(`DB CONNECTED ${name}`);
+    
+})("Boota")
