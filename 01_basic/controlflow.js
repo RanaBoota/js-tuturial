3ab9ddc6045482fab15157aaf8759ba92a19579a
Muhammad Boota <154687034+RanaBoota@users.noreply.github.com>
2025-09-04 06:54:36 +0000

    Control flow

---
+++ 01_basic/controlflow.js
@@ -0,0 +1,39 @@
+// if
+// const userloggedin=true
+// const temperature=40
+// if (temperature<50) {
+//     console.log("less than 50");
+    
+// }
+// else{
+// console.log("greater than 50");
+// }
+
+// const score=200
+// if (score>100) {
+//     const power="fly"
+//     console.log(`User power ${power}`);
+    
+// }
+
+
+
+
+
+// falsy values
+// false, 0, -0, BigInt 0n, "", undefined, null, NaN
+
+// truthy values
+// "0", " ", 'false', [], {}, function(){}
+
+
+
+// const useremail=[]
+// if (useremail.length===0) {
+//     console.log("Array is empty");
+    
+// }
+// const emptyobj={}
+// if (Object.keys(emptyobj).length===0) {
+//     console.log("Object is empty"); 
+// }
